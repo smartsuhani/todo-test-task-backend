1b80de505f1179a09a3fe31803f9331b05f0bf71
smartsuhani <suhanishah228@gmail.com>
2020-02-15 13:00:50 +0530

    add enviroment variable for mongo database

--- config/conn.js
+++ config/conn.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://root:root@cluster0-ovnuh.mongodb.net/tododb', { useNewUrlParser: true, useUnifiedTopology: true }).then(
+let mongoUrl = process.env.mongoUrl || 'mongodb+srv://root:root@cluster0-ovnuh.mongodb.net/tododb';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }).then(
   (success) => {
     console.log('Connection Sucessfully Done');
   },
