7cae1d6e2b6df0e841d69d9bfa69266d3646b3a7
smartsuhani <suhanishah228@gmail.com>
2020-02-15 13:23:19 +0530

    resolve the heroku port issue

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/todo', todoRouter);
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log('Server is Running on 5000');
 });
